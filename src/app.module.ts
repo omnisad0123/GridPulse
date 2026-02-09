0e454eb26ac11d4134e7b8fd07eade07cf4fc631
om.prakashnisad.ece20@itbhu.ac.in <omnisad0123>
2026-02-09 09:00:00 +0530

    chore: initialize nest backend structure

--- src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IngestModule } from './ingest/ingest.module';
-import { AnalyticsModule } from './analytics/analytics.module';
-
-@Module({
-  imports: [IngestModule, AnalyticsModule],
-})
-export class AppModule {}
--- src/ingest/ingest.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IngestController } from './ingest.controller';
-import { IngestService } from './ingest.service';
-
-@Module({
-  controllers: [IngestController],
-  providers: [IngestService],
-})
-export class IngestModule {}
