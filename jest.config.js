4532dccd5f812c0889afc1b6b52deee56596acc7
om.prakashnisad.ece20@itbhu.ac.in <omnisad0123>
2026-06-10 12:00:00 +0530

    test: include backend e2e specs in default suite

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   rootDir: './backend',
   testEnvironment: 'node',
-  testMatch: ['**/test/**/*.spec.ts'],
+  testMatch: ['**/test/**/*.spec.ts', '**/test/**/*.e2e-spec.ts'],
   moduleFileExtensions: ['js', 'json', 'ts'],
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest'
